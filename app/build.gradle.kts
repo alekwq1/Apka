3d1408c85bcc428735475c3e2f36d5dbaf838bf5
Aleksiej Malyszko <155390408+alekwq1@users.noreply.github.com>
2026-08-25 13:23:13 +0200

    Bump Android version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "pl.fujara.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "0.8.15"
+        versionCode = 33
+        versionName = "0.8.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
